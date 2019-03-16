6ea47d70e848fc60c470c756834a3864343ea331
Strix547 <strix547@mail.ru>
2019-03-16 20:02:05 +0300

    Updates

---
+++ precache-manifest.3c43d6b253d26d75e7a77902e517fe58.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/funbox-task/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "f9f0c1ed99b53ea6baba",
+    "url": "/funbox-task/static/css/main.e76030ac.chunk.css"
+  },
+  {
+    "revision": "277ba2dbce58282c53b8",
+    "url": "/funbox-task/static/js/runtime~main.a006d0f8.js"
+  },
+  {
+    "revision": "d7d04b9be0b2f05756e1",
+    "url": "/funbox-task/static/js/2.bbe43b75.chunk.js"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/funbox-task/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/funbox-task/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "/funbox-task/static/media/Robotomedium.4a34361b.woff2"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/funbox-task/static/media/Robotobold.15a913e1.woff2"
+  },
+  {
+    "revision": "f9f0c1ed99b53ea6baba",
+    "url": "/funbox-task/static/js/main.5e36c8a5.chunk.js"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/funbox-task/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/funbox-task/static/media/Robotomedium.6422eac7.woff"
+  },
+  {
+    "revision": "7bf74824d7a8b3c08fe5956f93e9ce2a",
+    "url": "/funbox-task/static/media/Robotobold.7bf74824.woff"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/funbox-task/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "/funbox-task/static/media/Robotomedium.38aa81d4.ttf"
+  },
+  {
+    "revision": "63d6f579b0db1fc75cc977a0539ce45f",
+    "url": "/funbox-task/static/media/Robotolight.63d6f579.ttf"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/funbox-task/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "901265f45a5689c953ff9b597ff95578",
+    "url": "/funbox-task/index.html"
+  }
+];
